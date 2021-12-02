f33486465857881c45070a81b61cae65cec39b45
hjga16 <hjga16@gmail.com>
2021-12-02 00:21:57 -0500

    New version of the site

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, render_template, request, flash, send_from_directory
 
 
@@ -5,17 +6,27 @@ app = Flask(__name__)
 app.secret_key = "pass123"
 
 
-@app.route("/profile")
+@app.route("/index")
 def index():
-	#flash("What's your name?")
-	flash("hernan.gavilan@outlook.com")
 	return render_template("index.html")
 
-@app.route("/greet", methods=['POST', 'GET'])
-def greeter():
-	flash("Hi " + str(request.form['name_input']) + ", great to see you!")
+@app.route("/about", methods=['GET', 'POST'])
+def about():
 	return render_template("index.html")
+
+@app.route("/resume", methods=['GET', 'POST'])
+def resume():
+	return render_template("resume.html")
+
 
 @app.route('/favicon.ico')
 def fav():
     return send_from_directory(os.path.join(app.root_path, 'static'),'icons/favicon.ico')
+
+
+
+
+
+##def greeter():
+##	flash("Hi " + str(request.form['name_input']) + ", great to see you!")
+##	return render_template("index.html")
